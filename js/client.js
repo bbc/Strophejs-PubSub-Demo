004d0ef7c5299f8d26578c898c5c26cce17a5ff6
duncan.robertson <duncan.robertson@4f02d5a8-cb4b-4bb5-80e5-e6ac929fdd9a>
2010-11-23 11:33:27 +0000

    add strophe demo
    git-svn-id: https://svn.rad0.net/repos/rad/xmpp/strophejs-pubsub-demo@22990 4f02d5a8-cb4b-4bb5-80e5-e6ac929fdd9a

---
+++ js/client.js
@@ -0,0 +1,130 @@
+var Client = {
+  pubsub_server: 'pubsub.' + Config.XMPP_SERVER,
+  connection: null,
+  subscribed: false,
+  show_raw: false,
+  show_log: true,
+
+  log: function (msg) { 
+    if (Client.show_log && window.console) { console.log(msg); }
+  },
+
+  raw_input: function (data)  {
+    if (Client.show_raw) {
+      Client.log('RECV: ' + data);
+    }
+  },
+
+  raw_output: function (data) { 
+    if (Client.show_raw) {
+      Client.log('SENT: ' + data);
+    }
+  },
+
+  feedback : function(msg, col) {
+    $('#connection_status').html(msg).css('color', col);
+  },
+
+  handle_update: function (data) {
+    var _d = $(data);
+    var _message = _d.html();
+    var _type = _d.attr('type'); 
+
+    switch (_type) {
+      case MessageType.MSG_TEXT:
+        Client.show_text(_message);
+        break;
+      case MessageType.MSG_HTML:
+        Client.show_html(_message);
+        break;
+      default:
+        Client.log("Oh dear! I don't understand");
+    }
+  },
+
+  show_text: function (m) {
+    $('#message').text(m);
+  },
+
+  show_html: function (m) {
+    $('#message').html(Client.html_decode(m));
+  },
+
+  html_decode: function(input) {
+    var e = document.createElement('div');
+    e.innerHTML = input;
+    return e.childNodes[0].nodeValue;
+  },
+
+  on_event: function (message) {
+    if (!Client.subscribed) {
+      return true;
+    }
+
+    var server = "^" + Client.pubsub_server.replace(/\./g, "\\.");
+    var re = new RegExp(server);
+
+    if ($(message).attr('from').match(re) && $(message).attr('type') == 'headline')
+    { 
+      var _data = $(message).children('event')
+        .children('items')
+        .children('item')
+        .children('entry').text();
+
+      if (_data) {
+        Client.handle_update(_data);
+      }
+    }
+
+    return true;
+  },
+
+  on_subscribe: function (sub) {
+    Client.subscribed = true;
+    Client.log("Now awaiting messages...");
+    Client.feedback('Connected', '#00FF00');
+
+    return true;
+  },
+
+  on_connect: function (status) {
+    if (status == Strophe.Status.CONNECTING) {
+      Client.log('Connecting...');
+      Client.feedback('Connecting... (1 of 2)', '#009900');
+    } else if (status == Strophe.Status.CONNFAIL) {
+      Client.log('Failed to connect!');
+      Client.feedback('Connection failed', '#FF0000');
+    } else if (status == Strophe.Status.DISCONNECTING) {
+      Client.log('Disconnecting...');
+      Client.feedback('Disconnecting...', '#CC6600');
+    } else if (status == Strophe.Status.DISCONNECTED) {
+      Client.log('Disconnected');
+      Client.feedback('Disconnected', '#aa0000');
+    } else if (status == Strophe.Status.CONNECTED) {
+      Client.log("Almost done...");
+      Client.feedback('Connecting... (2 of 2)', '#009900');
+      Client.connection.send($pres().c('priority').t('-1'));
+      Client.connection.pubsub.subscribe(
+        Client.connection.jid,
+	'pubsub.' + Config.XMPP_SERVER,
+        Config.PUBSUB_NODE,
+        [],
+        Client.on_event,
+        Client.on_subscribe
+      );
+    }
+    return true;
+  }
+}
+
+$(document).ready(function () {
+  var conn = new Strophe.Connection(Config.BOSH_SERVICE);
+  Client.connection = conn;
+  Client.connection.rawInput = Client.raw_input;
+  Client.connection.rawOutput = Client.raw_output;
+  Client.connection.connect(
+    Config.XMPP_SERVER + '/pubsub',
+    '',
+    Client.on_connect
+  );
+});
---
+++ js/publisher.js
@@ -0,0 +1,136 @@
+var Control = {
+  // being admin credentials
+  admin_jid: 'bbc@vm.local',
+  admin_pass: 'bbc',
+  // end admin credentials
+
+  pubsub_server: 'pubsub.' + Config.XMPP_SERVER,
+  connection: null,
+  connected: false,
+  show_raw: false,
+  show_log: true,
+
+  log: function (msg) { 
+    if (Control.show_log && window.console) {
+      console.log(msg);
+    }
+  },
+
+  status : function(msg, col) {
+    $('#connection_status').html(msg).css('color', col);
+  },
+  
+  raw_input: function (data)  { 
+    if (Control.show_raw) {
+      Control.log('RECV: ' + data);
+    }
+  },
+
+  raw_output: function (data) { 
+    if (Control.show_raw) {
+      Control.log('SENT: ' + data);
+    }
+  },
+
+  on_send: function (data) {
+    Control.log("Data Sent");
+
+    return true;
+  },
+
+  publish: function (data) {
+    var _d = $build('data', { 'type' : data.type }).t(data.message).toString(); 
+
+    Control.connection.pubsub.publish(
+      Control.admin_jid,
+      Control.pubsub_server,
+      Config.PUBSUB_NODE,
+      [_d],
+      Control.on_send
+    );
+  },
+
+  init: function () {
+    // send presence
+    Control.connection.send($pres());
+
+    // fetch the submit button
+    var _p = $('#publish');
+
+    // display the publish button
+    _p.fadeIn();
+
+    // setup the onlick event to send data
+    _p.click(function(event) {
+      event.preventDefault();
+
+      var _obj = {
+        'message' : $('textarea').val(),
+        'type'    : MessageType[$('input:radio:checked').val()]
+      }
+
+      Control.publish(_obj);    
+    });
+
+    return false;
+  },
+
+  on_create_node: function (data) {
+    Control.status('Connected', '#00FF00');
+    Control.init();
+  },
+}
+
+$(document).ready(function () {
+  Control.log('Ready to go...');
+  $(document).trigger('connect');
+});
+
+$(document).bind('connect', function () {
+  var conn = new Strophe.Connection(Config.BOSH_SERVICE);
+  Control.connection = conn;
+  Control.connection.rawInput = Control.raw_input;
+  Control.connection.rawOutput = Control.raw_output;
+  Control.connection.addHandler(Control.on_result, null, "message", null, null);
+  Control.connection.connect(
+    Control.admin_jid, Control.admin_pass, function (status) {
+      if (status == Strophe.Status.CONNECTING) {
+        Control.log('Connecting...');
+        Control.status('Connecting... (1 of 2)', '#009900');
+      } else if (status == Strophe.Status.CONNFAIL) {
+        Control.log('Failed to connect!');
+        Control.status('Connection failed', '#FF0000');
+      } else if (status == Strophe.Status.DISCONNECTING) {
+        Control.log('Disconnecting...');
+        Control.status('Disconnecting...', '#CC6600');
+      } else if (status == Strophe.Status.DISCONNECTED) {
+        Control.log('Disconnected');
+        Control.status('Disconnected', '#aa0000');
+        $(document).trigger('disconnected');
+      } else if (status == Strophe.Status.CONNECTED) {
+        $(document).trigger('connected');
+      }
+    }
+  );
+});
+
+$(document).bind('connected', function () {
+  Control.status('Connecting... (2 of 3)', '#00CC00');
+
+  // first we make sure the pubsub node exists
+  // buy trying to create it again
+  Control.connection.pubsub.createNode(
+    Control.admin_jid,
+    Control.pubsub_server,
+    Config.PUBSUB_NODE,
+    {},
+    Control.on_create_node
+  );
+});
+
+$(document).bind('disconnected', function () {
+  Control.log('Disconnected, goodbye');
+  $('#connection_status').html('Disconnected');
+  $('#connection_status').css('color', '#dd0000');
+});
+
